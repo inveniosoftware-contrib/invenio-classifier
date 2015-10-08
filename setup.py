d51f95fbe20ed26452843f52f2d4f30433a3354e
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2015-10-08 11:31:36 +0200

    installation: invenio-ext>=0.3.1
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requirements = [
     'Flask>=0.10.1',
     'six>=1.7.2',
     'invenio-base>=0.3.1',
-    'invenio-ext>=0.2.1',
+    'invenio-ext>=0.3.1',
     'invenio-records>=0.3.3',
     'invenio-utils>=0.1.1',
 ]
